6b1ddf217b6ce1349e1e1e82450ed28e3ca6a5ba
Isaac-Cannon <isaac.cannon1@gmail.com>
2020-04-16 20:36:31 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# sneakpeek
+
+This is a simple password checker created for practicing control structure, control flow and methods.
+
+## Requirements
+
+The latest version of Ruby.
---
+++ sneakpeek.rb
@@ -0,0 +1,36 @@
+class Sneakpeek
+
+attr_accessor :username
+
+    def initialize(username)
+        @username = username
+        @predefinedPassword = "Password1"
+        @attemptCounter = 5
+        @authenticated = false
+    end
+
+    def login
+        while !@authenticated 
+            puts "Welcome #{@username} Please enter your password to access the cake."
+            entered_password = gets.strip
+             if entered_password == @predefinedPassword
+            puts "The cake is yours #{@username}!"
+            @authenticated = true
+             else 
+            @attemptCounter = @attemptCounter - 1
+            puts "Incorrect password! #{@attemptCounter} attempts remaining."
+             end
+            if
+            @attemptCounter == 0
+            puts "You have been locked out of Sneakpeek. Please contact the administrator"
+            break
+            end
+        end
+    end
+
+
+end
+
+account = Sneakpeek.new("Chell")
+p account.username
+account.login
